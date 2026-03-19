0c9faa46b1aff70eb246ef04de0356f1bc37c6a6
Yamkela Mgcubhe <222040114@mycput.ac.za>
2026-03-19 23:00:17 +0200

    Merge branch 'master' of https://github.com/yamkela-mgcubhe24/ADP-Capstone

---
+++ src/main/java/za/ac/cput/factory/CustomerFactory.java
@@ -0,0 +1,29 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Customer;
+import za.ac.cput.util.Helper;
+
+public class CustomerFactory {
+    public static Customer createCustomer(String customerId,
+                                          String customerName,
+                                          String phoneNumber, String address){
+
+        if(Helper.isEmptyOrNull(customerId) ||
+           Helper.isEmptyOrNull(customerName) ||
+           Helper.isEmptyOrNull(phoneNumber) ||
+           Helper.isEmptyOrNull(address))
+        {
+            return null
+                    ;
+        }
+
+        return new Customer.Builder()
+                .setCustomerId(customerId)
+                .setCustomerName(customerName)
+                .setPhoneNumber(phoneNumber)
+                .setAddress(address)
+                .build();
+
+    }
+
+}
